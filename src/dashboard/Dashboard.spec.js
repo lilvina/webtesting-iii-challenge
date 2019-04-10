d3a7732699d86894a817603c4a66b7d2d80b2014
lilvina <davinaleo2007@yahoo.com>
2019-04-10 14:23:10 -0700

    working on tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,37 @@
 // Test away
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import 'jest-dom/extend-expect';
+import 'react-testing-library';
+
+import Dashboard from './Dashboard.js';
+import Controls from '../controls/Controls.js';
+import Display from '../display/Display.js';
+
+describe('<Dashboard />', () => {
+  it('renders successfully', () => {
+    render(<Dashboard />)
+  })
+
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+})
+
+describe('<Controls />', () => {
+  it('should render successfully', () => {
+    render(<Controls />)
+  })
+
+
+})
+
+describe('<Display />', () => {
+  it('should render successfully', () => {
+    render(<Display />)
+  })
+
+
+})
