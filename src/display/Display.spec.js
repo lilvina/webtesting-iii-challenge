45c8b7a493c079849a23f6c690ac292ffee6c288
lilvina <davinaleo2007@yahoo.com>
2019-04-10 15:27:31 -0700

    test passes for display spec

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -11,4 +11,27 @@ describe('<Display />', () => {
   it('should render without crashing', () => {
     render(<Display />)
   })
+
+  it('should display closed if prop is true', () => {
+    const { getByText } = render(<Display closed={true}/>)
+
+    expect(getByText(/closed/i))
+  })
+
+  it('should display lock if prop is true', () => {
+    const { getByText } = render(<Display locked={true}/>)
+
+    expect(getByText(/locked/i))
+  })
+
+  it('should display open if prop is false', () => {
+    const { getByText } = render(<Display closed={false}/>)
+
+    expect(getByText(/open/i))
+  })
+
+  it('should display unlock if prop is false', () => {
+    const {getByText} = render(<Display locked={false}/>)
+    expect(getByText(/unlocked/i))
+  })
 })
